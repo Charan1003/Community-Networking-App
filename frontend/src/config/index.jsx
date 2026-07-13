2e7aede721daa080e8d83ffb00c8d2a5aeb3d6d2
Charan1003 <charan1003g@gmail.com>
2026-07-13 14:04:43 +0530

    prod link

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://community-networking-app.onrender.com";
 export const clientServer = axios.create({
   baseURL: BASE_URL,
 });
